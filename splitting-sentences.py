af94be501f06cfdde6d6edb8ad35e8bf2e53bf71
munajboyapinni <49438763+munajboyapinni@users.noreply.github.com>
2019-07-10 11:30:11 +0000

    Add files via upload
    python code that enables you split a sentence into it's component words.

---
+++ splitting-sentences.py
@@ -0,0 +1,10 @@
+sentence = "the man is here"
+for i,word in enumerate(sentence.split()):
+ print ("word:" + word + " index: " + str(i))
+                        
+#word #0: the
+#word #1: man
+#word #2: is
+#word #3: here
+                
+                
